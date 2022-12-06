3c713c4e3f05c61a708a51db913935c17f6eb4dc
Isu Shin <isu010614@naver.com>
2022-12-06 16:01:36 +0900

    feat: 명령에 따른 기능 실행

--- src/main/java/subway/controller/PathController.java
+++ src/main/java/subway/controller/PathController.java
@@ -1,19 +1,29 @@
 package subway.controller;
 
+import subway.constant.ErrorMessage;
 import subway.domain.path.PathCriterion;
+import subway.domain.station.StationRepository;
 import subway.view.InputView;
 import subway.view.OutputView;
 
 import java.util.List;
 
 public class PathController {
-    public void run() {
+    public static void run() {
         printPathCriterionView();
         PathCriterion function = readFunction();
-
+        if (PathCriterion.byShortestDistance(function)) {
+            computeShortestDistancePath();
+        }
+        if (PathCriterion.byMinimumTime(function)) {
+            computeMinimumTimePath();
+        }
+        if (PathCriterion.isTurnBack(function)) {
+            MainController.viewMain();
+        }
     }
 
-    private void printPathCriterionView() {
+    private static void printPathCriterionView() {
         List<String> functions = PathCriterion.getFunctions();
         OutputView.printPathCriterionView(functions);
     }
@@ -28,4 +38,51 @@ public class PathController {
         }
     }
 
+    private static void computeShortestDistancePath() {
+        try {
+            String departureStation = readDepartureStation();
+            String arrivalStation = readArrivalStation();
+            validateStations(departureStation, arrivalStation);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            run();
+        }
+    }
+
+    private static void computeMinimumTimePath() {
+        try {
+            String departureStation = readDepartureStation();
+            String arrivalStation = readArrivalStation();
+            validateStations(departureStation, arrivalStation);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            run();
+        }
+    }
+
+    private static String readDepartureStation() {
+        try {
+            String stationName = InputView.inputDepartureStation();
+            return StationRepository.findByName(stationName);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return readDepartureStation();
+        }
+    }
+
+    private static String readArrivalStation() {
+        try {
+            String stationName = InputView.inputArrivalStation();
+            return StationRepository.findByName(stationName);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return readArrivalStation();
+        }
+    }
+
+    private static void validateStations(String departureStation, String arrivalStation) {
+        if (departureStation.equals(arrivalStation)) {
+            throw new IllegalArgumentException(ErrorMessage.SAME_STATION);
+        }
+    }
 }
