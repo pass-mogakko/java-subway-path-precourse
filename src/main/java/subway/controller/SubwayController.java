e58d80ce7fdf0d54bf03f2dbffaad5b7b8b46165
kjb95 <topmedia44@gmail.com>
2022-12-06 13:32:41 +0900

    feat: 스켈레톤 코드 완성

---
+++ src/main/java/subway/controller/SubwayController.java
@@ -0,0 +1,17 @@
+package subway.controller;
+
+import java.util.Scanner;
+import subway.view.InputView;
+
+public class SubwayController {
+
+    private final InputView inputView;
+
+    public SubwayController(Scanner scanner) {
+        inputView = new InputView(scanner);
+    }
+
+    public void run() {
+
+    }
+}
