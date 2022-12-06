cd7e8ff350cd48ccb6805fdb8ea02e0770c9720b
Isu Shin <isu010614@naver.com>
2022-12-06 14:11:18 +0900

    feat: 경로 기준 화면 출력 기능

--- src/main/java/subway/view/OutputView.java
+++ src/main/java/subway/view/OutputView.java
@@ -6,9 +6,18 @@ public class OutputView {
     private static final String INFO = "[INFO] %s" + System.lineSeparator();
 
     public static void printMainView(List<String> functions) {
+        System.out.print(System.lineSeparator());
         System.out.println("## 메인 화면");
         for(String function : functions){
-            System.out.printf(function);
+            System.out.println(function);
+        }
+    }
+
+    public static void printPathCriterionView(List<String> functions) {
+        System.out.print(System.lineSeparator());
+        System.out.println("## 경로 기준");
+        for(String function : functions){
+            System.out.println(function);
         }
     }
 }
