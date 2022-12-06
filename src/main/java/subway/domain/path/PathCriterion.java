2d76caa9c72a10f93c21a1d0e9d79379127b8019
Isu Shin <isu010614@naver.com>
2022-12-06 14:11:01 +0900

    feat: 경로 기준 기능에 대한 enum 생성 및 예외처리 구현

---
+++ src/main/java/subway/domain/path/PathCriterion.java
@@ -0,0 +1,59 @@
+package subway.domain.path;
+
+import subway.constant.ErrorMessage;
+import subway.domain.MainFunction;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
+public enum PathCriterion {
+    BY_SHORTEST_DISTANCE("1", "최단 거리"),
+    BY_MINIMUM_TIME("2", "최소 시간"),
+    TURN_BACK("B", "돌아가기");
+
+    private final String key;
+    private final String content;
+
+    PathCriterion(final String key, final String content) {
+        this.key = key;
+        this.content = content;
+    }
+
+    public static PathCriterion find(final String command) {
+        return Arrays.stream(PathCriterion.values())
+                .filter(function -> Objects.equals(function.getKey(), command))
+                .findAny()
+                .orElseThrow(() -> new IllegalArgumentException(ErrorMessage.NOT_EXIST_FUNCTION));
+    }
+
+    public static boolean byShortestDistance(PathCriterion criterion){
+        return criterion == BY_SHORTEST_DISTANCE;
+    }
+
+    public static boolean byMinimumTime(PathCriterion criterion){
+        return criterion == BY_MINIMUM_TIME;
+    }
+
+    public static boolean isTurnBack(PathCriterion command){
+        return command == TURN_BACK;
+    }
+
+    public static List<String> getFunctions() {
+        List<String> functions = new ArrayList<>();
+        for (MainFunction function : MainFunction.values()) {
+            functions.add(function.toString());
+        }
+        return functions;
+    }
+
+    public String getKey() {
+        return this.key;
+    }
+
+    @Override
+    public String toString() {
+        return key + ". " + content;
+    }
+}
