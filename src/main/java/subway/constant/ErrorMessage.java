2830481f2d4ca44c188a746d81f789984199bcb9
Isu Shin <isu010614@naver.com>
2022-12-06 16:01:48 +0900

    feat: 에러 메시지 추가

---
+++ src/main/java/subway/constant/ErrorMessage.java
@@ -0,0 +1,9 @@
+package subway.constant;
+
+public class ErrorMessage {
+    public static final String ERROR = "[ERROR]";
+    public static final String NOT_EXIST_FUNCTION = ERROR + "존재하지 않는 기능입니다.";
+    public static final String NOT_EXIST_LINE = ERROR + "존재하지 않는 노선입니다.";
+    public static final String NOT_EXIST_STATION = ERROR + "존재하지 않는 역입니다.";
+    public static final String SAME_STATION = ERROR + "출발역과 도착역이 달라야 합니다.";
+}
