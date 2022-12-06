9488bcd9520bbac29db0421f1e3bd52284b269ed
kjb95 <topmedia44@gmail.com>
2022-12-06 17:44:50 +0900

    feat: 출발지점과 도착지점이 같을때 예외처리

--- src/main/java/subway/service/PathLookupService.java
+++ src/main/java/subway/service/PathLookupService.java
@@ -1,12 +1,16 @@
 package subway.service;
 
 import java.util.List;
+import subway.constant.ErrorMessage;
 import subway.domain.section.SectionRepository;
 import subway.dto.PathDto;
 
 public class PathLookupService {
 
     public PathDto findShortestPath(String srcStation, String dstStation) {
+        if (srcStation.equals(dstStation)) {
+            throw new IllegalArgumentException(ErrorMessage.NOT_EXIST_SECTION);
+        }
         List<String> stations = SectionRepository.findShortestPathVertexList(srcStation, dstStation);
         int distance = SectionRepository.findShortestPathDistance(srcStation, dstStation);
         int time = SectionRepository.findShortestPathTime(stations);
@@ -14,6 +18,9 @@ public class PathLookupService {
     }
 
     public PathDto findMinimumTime(String srcStation, String dstStation) {
+        if (srcStation.equals(dstStation)) {
+            throw new IllegalArgumentException(ErrorMessage.NOT_EXIST_SECTION);
+        }
         List<String> stations = SectionRepository.findMinimumTimeVertexList(srcStation, dstStation);
         int time = SectionRepository.findMinimumTime(srcStation, dstStation);
         int distance = SectionRepository.findMinimumTimeDistance(stations);
