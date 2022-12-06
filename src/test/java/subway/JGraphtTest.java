b0182628917992608e4cd89fb87ae4dbc644dded
kjb95 <topmedia44@gmail.com>
2022-12-06 13:28:41 +0900

    style: 코드 자동정렬

--- src/test/java/subway/JGraphtTest.java
+++ src/test/java/subway/JGraphtTest.java
@@ -1,15 +1,15 @@
 package subway;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
 import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
 import org.jgrapht.graph.DefaultWeightedEdge;
 import org.jgrapht.graph.WeightedMultigraph;
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
-
-import static org.assertj.core.api.Assertions.assertThat;
-
 public class JGraphtTest {
+
     @Test
     public void getDijkstraShortestPath() {
         WeightedMultigraph<String, DefaultWeightedEdge> graph = new WeightedMultigraph(DefaultWeightedEdge.class);
@@ -21,7 +21,8 @@ public class JGraphtTest {
         graph.setEdgeWeight(graph.addEdge("v1", "v3"), 100);
 
         DijkstraShortestPath dijkstraShortestPath = new DijkstraShortestPath(graph);
-        List<String> shortestPath = dijkstraShortestPath.getPath("v3", "v1").getVertexList();
+        List<String> shortestPath = dijkstraShortestPath.getPath("v3", "v1")
+                .getVertexList();
 
         assertThat(shortestPath.size()).isEqualTo(3);
     }
